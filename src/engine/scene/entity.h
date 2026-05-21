14bc9f0ee11e125a084208b07f55324296771ece
Ernest <ernest326648@gmail.com>
2026-05-21 23:47:47 +0100

    For some reason changes didnt push, entity impl :p

---
+++ src/engine/scene/entity.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <entt.hpp>
+
+namespace Karbon {
+
+class Entity {
+public:
+    Entity() = default;
+    Entity(entt::entity handle, entt::registry* registry);
+    Entity(const Entity& other) = default;
+
+    template<typename T, typename... Args>
+    T& AddComponent(Args&&... args) {
+        return m_Registry->emplace<T>(m_EntityHandle, std::forward<Args>(args)...);
+    }
+
+    template<typename T>
+    T& GetComponent() {
+        return m_Registry->get<T>(m_EntityHandle);
+    }
+
+    template<typename T>
+    bool HasComponent() {
+        return m_Registry->any_of<T>(m_EntityHandle);
+    }
+
+private:
+    entt::entity m_EntityHandle{entt::null};
+    entt::registry* m_Registry{nullptr};
+};
+
+}
